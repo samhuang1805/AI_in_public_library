98d3a0441cdb86e3e8cea25d7a51260ed6b0a6fc
samhuang1805 <165962103+samhuang1805@users.noreply.github.com>
2024-04-05 23:32:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,17 +91,9 @@ for (lat, lng), group in grouped:
 # Display Folium map in Streamlit with dynamic full-width
 st_folium(library_map, width='100%', height=600)
 
-# Create table for input
-creds_json = st.secrets["gcp_service_account"]
-
-# Use creds to create a client to interact with the Google Drive API
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_json, scope)
-client = gspread.authorize(creds)
 
 # Open the sheet
-sheet = client.open("AI-library-response-sheet").sheet1  # Name of your Google Sheet
+sheet = []
 
 # Create input fields in the sidebar
 with st.sidebar:
