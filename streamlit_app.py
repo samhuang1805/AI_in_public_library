1c3fe9e4e47c46b167bd6b91de8d1fd62695f0df
Zong-Xian Huang <165962103+samhuang1805@users.noreply.github.com>
2024-07-23 15:13:49 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,7 +108,7 @@ for (lat, lng), group in grouped:
 
 
 # Display Folium map in Streamlit with dynamic full-width
-folium_static(library_map, width='100%', height=950)
+folium_static(library_map, width=900, height=900)
 
 
 # Creating an interactive table to input data
