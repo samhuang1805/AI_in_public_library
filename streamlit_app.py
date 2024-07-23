ede4da0cd17521b9a17c0edca75999df6ac54125
Zong-Xian Huang <165962103+samhuang1805@users.noreply.github.com>
2024-07-23 22:19:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,7 @@ social_links_html = """
     </a>
     <a href='https://www.urbanlibraries.org/' target='_blank'>
         <img src='https://www.infodocket.com/wp-content/uploads/2022/01/logo_dark_text.png' alt='ULC-logo' style='width:128px;height:64px;margin:0 10px;'>
+    </a>
 </div>
 """
 st.markdown(social_links_html, unsafe_allow_html=True)
@@ -142,10 +143,9 @@ if submit_button:
 # Adding social media links with icons
 social_links_html = """
 <div style='text-align: center;'>
-    <a href='https://www.ctg.albany.edu/' target='_blank'>
-        <img src='https://media.licdn.com/dms/image/D4E0BAQFVSVlZhcDIFw/company-logo_200_200/0/1693395291995?e=2147483647&v=beta&t=CWHo7yPFoN2eyabf5o4jb40sqSiT7EZkAtL8_9PCHtI' alt='CTG-logo' style='width:128px;height:128px;margin:0 10px;'>
-    <a href='https://www.urbanlibraries.org/' target='_blank'>
-        <img src='https://www.infodocket.com/wp-content/uploads/2022/01/logo_dark_text.png' alt='ULC-logo' style='width:128px;height:64px;margin:0 10px;'>
+    <a href='https://ctgualbany.my.canva.site/imls-website' target='_blank'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/7/70/Applications-internet.svg' alt='homepage' style='width:48px;height:48px;margin:0 10px;'>
+    </a>
     <a href='https://twitter.com/CTGUAlbany' target='_blank'>
         <img src='https://upload.wikimedia.org/wikipedia/commons/5/53/X_logo_2023_original.svg' alt='X-logo' style='width:48px;height:48px;margin:0 10px;'>
     </a>
